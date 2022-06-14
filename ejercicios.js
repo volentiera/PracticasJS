78e31f1bf75d07bf4bb8b755620adab404a55e10
volentiera <santiagovolentiera@gmail.com>
2022-06-14 17:29:30 -0300

    cambio chico

--- ejercicios.js
+++ ejercicios.js
@@ -2,6 +2,8 @@
 // multiplica un numero con otro al ingresar ambos, y continua si el total es menor a 100.
 
 let total = 0
+let numero = 0
+let multiplica = 0
 
 while (total<= 100){
     let numero = parseInt(prompt("Ingrese un numero: "))
