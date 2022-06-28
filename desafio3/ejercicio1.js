35d304839f89c0514dc040d1f4b05ba44911ccd3
volentiera <santiagovolentiera@gmail.com>
2022-06-27 23:12:22 -0300

    desa3

--- desafio3/ejercicio1.js
+++ desafio3/ejercicio1.js
@@ -42,7 +42,7 @@ class Pack {
         this.precio = precio
     }
     iva = () => this.precio * 0.21
-    sumatoriaTotal = () => this.precio
+    sumatoriaTotal = () => this.precio * iva()
     
 }
 
@@ -105,64 +105,7 @@ function sumatoriaTotal(packs){
     return sumatoriaFinal
 }
 
-/*
-function menu(){
-    let ingresoProducto = parseInt(prompt("Ingrese su numero correspondiente \n 1-Pack Beginner \n 2-Pack Advanced \n 3-Pack Premium \n 4-Pack Pro  "))
-    if ((ingresoProducto !== "") && (ingresoProducto <= 4) && (ingresoProducto > 0)) {
-        switch (ingresoProducto) {
-            case 1:
-                producto = 500
-                total = producto + iva(producto)
-                alert("Su producto elegido es: " + " Pack Beginner " + "El valor es: $ " + total)
-                break
-            case 2:
-                producto = 800
-                total = producto + iva(producto)
-                alert("Su producto elegido es: " + " Pack Advanced " + "El valor es: $ " + total)
-                break
-            case 3:
-                producto = 1350
-                total = producto + iva(producto)
-                alert("Su producto elegido es: " + " Pack Premium " + "El valor es: $ " + total)
-                break
-            case 4:
-                producto = 1680
-                total = producto + iva(producto)
-                alert("Su producto elegido es: " + " Pack Pro " + "El valor es: $ " + total)
-                break
-        }
-    } else {
-        alert("ingrese un numero de producto")
-        menu()
-    }
-}
-*/
-/*function pago() {
-    let ingresoPago = parseInt(prompt("Ingrese su numero correspondiente \n 1-Pago en 6 cuotas sin interes \n 2-En efectivo 10% descuento \n 3-50% efectivo el resto en 24 cuotas con 10% recargo"))
-    if ((ingresoPago !== "") && (ingresoPago <= 3) && (ingresoPago > 0)){
-        switch (ingresoPago){
-            case 1:
-                let totalEnCuotas = seisCuotas(total)
-                alert("El total es: $" + totalEnCuotas + " Por cuota")
-                break
-            case 2:
-                let totalEfectivo = producto - porcentaje(total)
-                alert("El total es: $" + totalEfectivo)
-                break
-            case 3:
-                let totalEfectivo2 = mitadEfectivo(total)
-                let totalEnCuotas2 = doceCuotas(total)
-                alert("El total es : \n" + "$ " + totalEfectivo2 + " en un pago\n" + "$ " + totalEnCuotas2 + " En 12 cuotas" )
-                break
-        }
-    } else {
-        
-        alert("ingrese un numero del 1 al 3 para seleccionar su forma de pago")
-        pago()
-        
-    }
-}
-*/
+
 
 //programa
 
